bc1600ab082d37462c1566e71f12294d2d3b28a6
Adan.fig <jorgeot@uamv.edu.ni>
2026-09-08 16:21:08 -0600

    update: Cargo Empleado

--- src/main/java/ni/edu/uam/facapp/model/Empleado.java
+++ src/main/java/ni/edu/uam/facapp/model/Empleado.java
@@ -1,4 +1,26 @@
 package ni.edu.uam.facapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Empleado {
+    private Integer id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private Cargo cargo;
+
+    private LocalDate fechaContratacion;
+
+    private boolean activo;
+
+
 }
